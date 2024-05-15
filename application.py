d3db74cd699182bc7511f3204c1afa0e9074396e
Shubhankar5002 <100302996+Shubhankar5002@users.noreply.github.com>
2024-05-15 23:49:58 +0530

    Deployment config

--- application.py
+++ application.py
@@ -35,4 +35,6 @@ def predict_datapoint():
         return render_template('home.html',results=prediction[0])
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
+
+    
